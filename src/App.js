cf0474e67c56a238772a11591b977fcfaedcc6d1
Алешенька:3 <alexnagorny.an@gmail.com>
2019-03-24 18:29:31 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import ranges from './ranges'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+
+    render() {
+        return (
+            <div className="App">
+                <img src={require('./image.jpg')} alt={'test'}/>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1279.5px" height="853.5px">
+                    {ranges["testHotel"].map(range =>
+                        <path
+                            fillRule="evenodd"
+                            opacity="0.2"
+                            onClick={() => alert(`floor: ${range.floor} ${range.side}`)}
+                            fill="rgb(0, 0, 204)"
+                            d={range.mapContent}
+                        />)}
+                </svg>
+            </div>
+        );
+    }
 }
 
 export default App;
